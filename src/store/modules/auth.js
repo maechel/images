dfeac98d06d83b59db42afafb91f087059c159b2
Lars Maechel <lars.maechel@gmail.com>
2018-05-15 10:46:52 +0200

    Add login to auth module

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,3 +1,5 @@
+import api from '../../api/imgur';
+
 const state = {
     token: null
 };
@@ -14,9 +16,14 @@ const mutations = {
 };
 
 const actions = {
+    login: () => {
+        api.login();
+    },
+
     logout: ({ commit }) => {
         commit('setToken', null);
     }
+
 };
 
 export {
