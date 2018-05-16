232f8b8a1be779aae907dc75a774cd2548ad6cbb
Lars Maechel <lars.maechel@gmail.com>
2018-05-16 19:26:37 +0200

    Use router-link for navigation within application

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="ui secondary pointing menu">
-        <a href="/" class="active item">
+        <router-link to="/" class="active item">
             Image Storage
-        </a>
+        </router-link>
         <div class="right menu">
             <div v-if="isLoggedIn" class="horizontal">
-                <a href="#" class="item">Gallery</a>
-                <a href="#" class="item">Upload</a>
-                <a href="#" class="item" @click="logout">Logout</a>
+                <router-link to="/" class="item">Gallery</router-link>
+                <router-link to="/upload" class="item">Upload</router-link>
+                <a class="item" @click="logout">Logout</a>
             </div>
             <a v-else href="#" class="menu item" @click="login">Login</a>
         </div>
