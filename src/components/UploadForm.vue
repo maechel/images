0d34fa9ba5cbec6c4edbb15457b31f7f003cd230
Lars Maechel <lars.maechel@gmail.com>
2018-05-17 10:38:11 +0200

    Styling the UploadForm and the apply class container to overall App

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        UploadForm
+    <div class="dropper">
+        <input type="file" />
+        <span>Drop image here to upload it</span>
     </div>
 </template>
 
@@ -11,5 +12,24 @@
 </script>
 
 <style scoped>
-
+    .dropper {
+        height: 30vh;
+        border: 2px dashed black;
+        border-radius: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+    }
+    
+    .dropper:hover {
+        background-color: #eee;
+    }
+    
+    input {
+        width: 100%;
+        height: 30vh;
+        opacity: 0;
+        position: absolute;
+    }
 </style>
